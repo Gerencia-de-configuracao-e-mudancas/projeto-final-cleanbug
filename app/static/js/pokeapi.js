2257c28bcd98ad4bdc4e0988a6b128881552eddf
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-12-11 12:46:37 -0300

    alterações no detail.html

--- app/static/js/pokeapi.js
+++ app/static/js/pokeapi.js
@@ -128,69 +128,68 @@ function createPokemonCard(pokemon) {
     linkElement.addEventListener('click', handlePokemonClick);
 }
 
-async function getEvolutionChain(pokemonId) {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokemonId}`);
-        const data = await response.json();
+// async function getEvolutionChain(pokemonId) {
+//     try {
+//         const response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokemonId}`);
+//         const data = await response.json();
 
-        const evolutions = extractEvolutions(data.chain);
+//         const evolutions = extractEvolutions(data.chain);
 
-        // Adiciona as imagens das evoluções no HTML
-        const evolutionContainer = document.querySelector('.evolutions');
-        evolutionContainer.innerHTML = '';
+//         // Adiciona as imagens das evoluções no HTML
+//         const evolutionContainer = document.querySelector('.evolutions');
+//         evolutionContainer.innerHTML = '';
 
-        for (const evolution of evolutions) {
-            await addEvolutionImage(evolution, evolutionContainer);
-        }
-    } catch (error) {
-        console.error('Error fetching evolution chain:', error);
-    }
-}
+//         for (const evolution of evolutions) {
+//             await addEvolutionImage(evolution, evolutionContainer);
+//         }
+//     } catch (error) {
+//         console.error('Error fetching evolution chain:', error);
+//     }
+// }
 
-async function addEvolutionImage(evolution, container) {
-    return new Promise((resolve, reject) => {
-        const imgElement = document.createElement('img');
-        imgElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${evolution.id}.png`;
-        imgElement.alt = evolution.name;
+// async function addEvolutionImage(evolution, container) {
+//     return new Promise((resolve, reject) => {
+//         const imgElement = document.createElement('img');
+//         imgElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${evolution.id}.png`;
+//         imgElement.alt = evolution.name;
 
-        imgElement.addEventListener('load', () => {
-            const linkElement = document.createElement('a');
-            linkElement.href = `#`; // Você pode adicionar um link apropriado se necessário
-            linkElement.appendChild(imgElement);
+//         imgElement.addEventListener('load', () => {
+//             const linkElement = document.createElement('a');
+//             linkElement.href = `#`; // Você pode adicionar um link apropriado se necessário
+//             linkElement.appendChild(imgElement);
 
-            container.appendChild(linkElement);
-            resolve();
-        });
+//             container.appendChild(linkElement);
+//             resolve();
+//         });
 
-        imgElement.addEventListener('error', () => {
-            reject(`Erro ao carregar imagem para ${evolution.name}`);
-        });
-    });
-}
+//         imgElement.addEventListener('error', () => {
+//             reject(`Erro ao carregar imagem para ${evolution.name}`);
+//         });
+//     });
+// }
 
-function extractEvolutions(chain) {
-    const evolutions = [];
-    let current = chain;
+// function extractEvolutions(chain) {
+//     const evolutions = [];
+//     let current = chain;
 
-    while (current) {
-        const pokemon = current.species;
-        evolutions.push({
-            id: extractPokemonIdFromUrl(pokemon.url),
-            name: pokemon.name
-        });
+//     while (current) {
+//         const pokemon = current.species;
+//         evolutions.push({
+//             id: extractPokemonIdFromUrl(pokemon.url),
+//             name: pokemon.name
+//         });
 
-        current = current.evolves_to[0]; // Assume que só há uma evolução em cada nível da cadeia
-    }
+//         current = current.evolves_to[0]; // Assume que só há uma evolução em cada nível da cadeia
+//     }
 
-    return evolutions;
-}
+//     return evolutions;
+// }
 
-function extractPokemonIdFromUrl(url) {
-    const matches = url.match(/\/(\d+)\/$/);
-    return matches ? parseInt(matches[1]) : null;
-}
+// function extractPokemonIdFromUrl(url) {
+//     const matches = url.match(/\/(\d+)\/$/);
+//     return matches ? parseInt(matches[1]) : null;
+// }
 
-// Adiciona um listener para o clique no evento
 function handlePokemonClick(event) {
     event.preventDefault();
     
@@ -208,8 +207,7 @@ function handlePokemonClick(event) {
         })
         .catch(error => console.error('Erro ao obter detalhes do Pokémon:', error));
     
-    // Adiciona a chamada para obter a cadeia de evolução
-    //getEvolutionChain(pokemonId);
+    // getEvolutionChain(pokemonId);
 }
 
 getAllPokemons();
